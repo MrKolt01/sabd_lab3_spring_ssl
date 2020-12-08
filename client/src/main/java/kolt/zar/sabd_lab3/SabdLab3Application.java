2cbfe05167e79528ecbf0ec42017882cc14c9121
mrkolt01 <kolt.zar@mail.ru>
2020-12-08 17:34:27 +0300

    Initial commit

---
+++ client/src/main/java/kolt/zar/sabd_lab3/SabdLab3Application.java
@@ -0,0 +1,13 @@
+package kolt.zar.sabd_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabdLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabdLab3Application.class, args);
+	}
+
+}
